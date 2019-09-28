4a02d26f9cc4c378f8f970fb9bbe8e5ce27d1184
technateNG <technate@protonmail.com>
2019-09-28 19:43:30 +0000

    Refactor in main.

--- firmware/main.c
+++ firmware/main.c
@@ -6,20 +6,27 @@
 #include <string.h>
 #include <stdbool.h>
 #include "usbdrv.h"
-#include "usb_hid_keys.h"
 #include "keyboard_device.h"
 #include "loader_device.h"
 
 #define NO_RETURN __attribute__((noreturn))
 #define ALWAYS_INLINE __attribute__((always_inline))
 #define PAYLOAD_BUFFER_LEN 512
-#define END_OF_STREAM 0xff
+#define ONE_SECOND 15625
 
-typedef enum
+typedef enum Mode
 {
     LOADER = 0,
     KEYBOARD = 0xff
 } mode_t;
+
+typedef enum Token
+{
+
+    MODIFIER = 0x01,
+    TIME = 0x02,
+    END_OF_STREAM = 0xff,
+} token_t;
 
 static mode_t mode;
 static uint16_t eeprom_write_pos;
@@ -86,7 +93,27 @@ static inline void usb_device_restart()
 }
 
 ALWAYS_INLINE
-static inline uint8_t check_mode()
+static inline void fill_report_with_zeroes()
+{
+    memset(&keyboard_report, 0, sizeof(keyboard_report_t));
+}
+
+ALWAYS_INLINE
+static inline void stop_clock()
+{
+    TIFR |= (1 << OCF1A);
+    TCCR1B = 0;
+}
+
+ALWAYS_INLINE
+static inline void start_clock()
+{
+    TCCR1B = (1 << CS10) | (1 << CS12);
+    TCNT1 = 0;
+}
+
+ALWAYS_INLINE
+static inline mode_t check_mode()
 {
     return bit_is_set(PINC, PC1) ? KEYBOARD : LOADER;
 }
@@ -94,12 +121,12 @@ static inline uint8_t check_mode()
 NO_RETURN
 int main()
 {
-    PORTC |= 1u << PC1;
+    PORTC |= 1 << PC1;
     eeprom_busy_wait();
     wdt_enable(WDTO_1S);
 
     mode = check_mode();
-    if (mode)
+    if (mode == LOADER)
     {
         eeprom_read_block(payload_buffer, (uint8_t*) 0, PAYLOAD_BUFFER_LEN);
     }
@@ -114,12 +141,12 @@ int main()
     {
         wdt_reset();
         usbPoll();
-        const uint8_t actual_mode_flag = check_mode();
+        const mode_t actual_mode_flag = check_mode();
         if (actual_mode_flag != mode)
         {
             usb_device_restart();
             mode = actual_mode_flag;
-            if (mode)
+            if (mode == LOADER)
             {
                 eeprom_read_block(payload_buffer, (uint8_t*) 0, PAYLOAD_BUFFER_LEN);
             }
@@ -127,32 +154,30 @@ int main()
         if (TIFR & (1 << OCF1A))
         {
             can_interrupt = true;
-            TIFR |= (1 << OCF1A);
-            TCCR1B = 0;
+            stop_clock();
         }
         if (mode && can_interrupt && usbInterruptIsReady())
         {
             if (send_zero_report)
             {
-                memset(&keyboard_report, 0, sizeof(keyboard_report_t));
+                fill_report_with_zeroes();
                 usbSetInterrupt((void*) &keyboard_report, sizeof(keyboard_report_t));
                 send_zero_report = false;
             }
-            else if (payload_buffer[eeprom_read_pos] == 0x02)
+            else if (payload_buffer[eeprom_read_pos] == TIME)
             {
-                TCCR1B = (1 << CS10) | (1 << CS12);
-                OCR1A = payload_buffer[eeprom_read_pos + 1] * 15625;
-                TCNT1 = 0;
+                OCR1A = payload_buffer[eeprom_read_pos + 1] * ONE_SECOND;
+                start_clock();
                 can_interrupt = false;
                 eeprom_read_pos += 2;
             }
-            else if (payload_buffer[eeprom_read_pos] == 0xff)
+            else if (payload_buffer[eeprom_read_pos] == END_OF_STREAM)
             {
                 can_interrupt = false;
             }
             else
             {
-                if (payload_buffer[eeprom_read_pos] == 0x01)
+                if (payload_buffer[eeprom_read_pos] == MODIFIER)
                 {
                     keyboard_report.modifier = payload_buffer[eeprom_read_pos + 1];
                     eeprom_read_pos += 2;
